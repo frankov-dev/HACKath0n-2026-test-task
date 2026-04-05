eaeddf1c210039b065cf1f24237397c416eab26b
Dmytro <dmytro.frankov@gmail.com>
2026-04-05 18:01:39 +0300

    Add API documentation endpoints and remove CommonMiddleware from settings

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import WarehouseViewSet, DeliveryPointViewSet, RequestViewSet
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
 
 # Створюємо роутер, який сам збудує правильні шляхи
 router = DefaultRouter()
@@ -13,4 +15,8 @@ router.register(r'requests', RequestViewSet, basename='request')
 urlpatterns = [
     # Всі маршрути будуть доступні за префіксом, який ми вкажемо в головному urls.py
     path('', include(router.urls)),
+
+    # Документація
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
--- core/settings.py
+++ core/settings.py
@@ -54,7 +54,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
